cdc2064e7982f8533bcc0f9e25a7033b2fa46dbd
Yuizhey <Gre4ka28@yandex.ru>
2024-11-29 05:34:46 +0300

    добавил и стилизовал начальный экран добавил тёмную тему добавил сохранение настроек темы пользователя

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import '../css/style.css'
+import { darkModeHandle } from './utils';
+
+darkModeHandle(); 
+
+const startGameButton = document.getElementById('startGame');
+startGameButton.addEventListener('click',()=>{
+    
+})
